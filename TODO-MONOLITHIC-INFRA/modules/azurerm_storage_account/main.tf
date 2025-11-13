1a3cec0e2dd14d9989b37d155fdd9b7d4a2a87ab
Pardeepkumar <pardeepkumar02610@gmail.com>
2025-11-14 01:26:02 +0530

    RG & STG code

---
+++ TODO-MONOLITHIC-INFRA/modules/azurerm_storage_account/main.tf
@@ -0,0 +1,16 @@
+resource "azurerm_storage_account" "stg"{
+    for_each=var.stg
+    name=each.value.name
+    location=each.value.location
+  resource_group_name =each.value.resource_group_name 
+  account_tier=each.value.account_tier
+  account_replication_type=each.value.account_replication_type
+  dynamic "network_rules"{
+    for_each=each.value.network_rules
+    content{
+    default_action=network_rules.value.default_action
+      bypass=network_rules.value.bypass
+      ip_rules=network_rules.value.ip_rules
+    }
+  }
+}
---
+++ TODO-MONOLITHIC-INFRA/modules/azurerm_storage_account/variable.tf
@@ -0,0 +1,16 @@
+variable "stg"{
+  type=map(object({
+    name=string
+    location=string
+    resource_group_name=string
+    account_tier=string
+    account_replication_type=string
+    network_rules=map(object({
+      default_action=string
+      bypass=list(string)
+      ip_rules=list(string)
+    }))
+}))
+}
+
+
